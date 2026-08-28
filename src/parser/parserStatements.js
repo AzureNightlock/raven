b0092f0c91ff3052674fdcef45ba71c0eacf590a
Aaron Benmohan John <aaronjoanajohn@gmail.com>
2026-08-28 17:52:38 +0530

    impr: change identifier error message

--- src/parser/parserStatements.js
+++ src/parser/parserStatements.js
@@ -38,7 +38,7 @@ export function parseStatement(stream) {
     throw new RavenError(
       `Expected a property assignment or event handler after "${token.value}`,
       token,
-      `Write a html <property> = value or <event>(() => { ... }).`,
+      `Write a html <property> = value or <event>({ ... }).`,
     );
   }
 
