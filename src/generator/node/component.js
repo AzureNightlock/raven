35ba3f61fc240b903773f512c699785ee28be1be
Aaron Benmohan John <aaronjoanajohn@gmail.com>
2026-09-15 07:37:30 +0530

    add component

--- src/generator/node/component.js
+++ src/generator/node/component.js
@@ -3,8 +3,16 @@ import { generateNode } from "../generate.js";
 export function generateComponent(node, lines) {
   lines.push(`function ${node.varName}() {`);
 
+  const elements = [];
+
   for (const statement of node.body) {
     generateNode(statement, lines, null);
+
+    if (statement.type === "CreateHTMLElement") {
+      elements.push(statement.varName);
+    }
   }
-  lines.push("}");
+
+  lines.push(`return [${elements.join(", ")}];`);
+  lines.push(`}`);
 }
--- src/generator/node/componentAssignment.js
+++ src/generator/node/componentAssignment.js
@@ -2,8 +2,10 @@ export function generateComponentAssignment(node, lines, currentElement) {
   lines.push(`const ${node.varName} = ${node.component}();`);
 
   if (currentElement !== null) {
-    lines.push(
-      `${currentElement ?? "document.body"}.appendChild(${node.varName});`,
-    );
+    const parent = currentElement ?? "document.body";
+
+    lines.push(`for (const element of ${node.varName}) {`);
+    lines.push(`  ${parent}.appendChild(element);`);
+    lines.push(`}`);
   }
 }
